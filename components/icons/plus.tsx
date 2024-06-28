1c75cde1a912312da35a5e8e6e1573372257de81
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-06-28 01:14:02 -0600

    Add new icons for hero section

---
+++ components/icons/plus.tsx
@@ -0,0 +1,22 @@
+import { SVGProps } from 'react'
+
+export function PlusIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='1em'
+      height='1em'
+      viewBox='0 0 24 24'
+      {...props}
+    >
+      <path
+        fill='none'
+        stroke='currentColor'
+        stroke-linecap='round'
+        stroke-linejoin='round'
+        stroke-width='1.5'
+        d='M6 12h6m6 0h-6m0 0V6m0 6v6'
+      />
+    </svg>
+  )
+}
