a91577e45bf39e058c01db74706652c66c963d4c
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-06-27 22:31:51 -0600

    future landing sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from '@/components/Header'
 import { Hero } from '@/components/landing/Hero'
+import { Marquee } from '@/components/landing/Marquee'
 
 export default async function Index() {
   return (
     <div className=''>
       <Header />
       <Hero />
+      <Marquee />
     </div>
   )
 }
