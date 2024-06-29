eff104a5b7ec7dd7aabbb9a2f1a92701ad7a910a
Fabian Montoya <fabian.montoya2410@gmail.com>
2024-06-29 14:28:15 -0600

    refactor: Update landing page structure to use Container component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 import Header from '@/components/Header'
+import { Container } from '@/components/container'
 import { Hero } from '@/components/landing/Hero'
+import { FeaturedProducts } from '@/components/landing/featured-products'
 import { TextOffers } from '@/components/landing/text-offers'
 
 export default async function Index() {
   return (
-    <div>
-      <Header />
-      <Hero />
+    <>
+      <Container>
+        <Header />
+      </Container>
+      <div className='bg-gradient-to-r from-gray-100 via-stone-100 to-blue-100'>
+        <Container>
+          <Hero />
+        </Container>
+      </div>
       <TextOffers />
-    </div>
+      <Container>
+        <FeaturedProducts />
+      </Container>
+    </>
   )
 }
